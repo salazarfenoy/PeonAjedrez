ce1dbfe5b3079fe902ec3aee73d6d953987ae47b
salaz <salaz@DESKTOP-IVOPF5P>
2019-10-29 16:36:51 +0100

    Posicion getters and setters

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Posicion.java
@@ -1,8 +1,35 @@
 package org.iesalandalus.programacion.peonajedrez;
 
 public class Posicion {
-	
+
 	private int fila;
 	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+
+		if (fila > 0 || fila < 9) {
+			this.fila = fila;
+		} else {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+
+		}
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (columna == 'a' || columna == 'b' || columna == 'c' || columna == 'd' || columna == 'e' || columna == 'f'
+				|| columna == 'g' || columna == 'h') {
+			this.columna = columna;
+		} else {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+	}
 
 }
