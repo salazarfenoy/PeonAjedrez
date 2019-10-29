36f44e80c2a34a33e508958dc9e94b58ccbef001
salaz <salaz@DESKTOP-IVOPF5P>
2019-10-29 22:03:54 +0100

    getters y setters

--- src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
+++ src/main/java/org/iesalandalus/programacion/peonajedrez/Peon.java
@@ -3,4 +3,23 @@ package org.iesalandalus.programacion.peonajedrez;
 public class Peon {
 	private Color color;
 	private Posicion posicion;
+	
+	
+	
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		if (color==null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		if (color == Color.BLANCO || color == Color.NEGRO) {
+		this.color = color;}
+		}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 }
